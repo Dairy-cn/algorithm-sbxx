6d128a6c1ef1cba8f18c7dfe33bef2b68f8e04db
dair <2997266712@qq.com>
2021-04-27 17:30:04 +0800

    删除排序链表的重复数据

---
+++ src/main/java/com/dair/sbxx/day4/Solution.java
@@ -0,0 +1,22 @@
+package com.dair.sbxx.day4;
+
+/**
+ * @author Dair
+ * @since 爬楼梯
+ */
+public class Solution {
+	
+	public int climbStairs(int x) {
+		int p = 0;
+		int q = 0;
+		int r = 1;
+		for (int i = 1; i <= x; i++) {
+			p = q;
+			q = r;
+			r = q + p;
+		}
+		return r;
+		
+	}
+	
+}
---
+++ src/test/java/com/dair/sbxx/day4/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.dair.sbxx.day4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Dair
+ * @since
+ */
+class SolutionTest {
+	
+	@Test
+	void climbStairs() {
+		Solution solution=new Solution();
+		System.out.println(solution.climbStairs(10));
+	}
+}
