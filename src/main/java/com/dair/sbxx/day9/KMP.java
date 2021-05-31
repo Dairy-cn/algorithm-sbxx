7ac636561563ca789fcc50e6579c22bf00a08af9
dair <2997266712@qq.com>
2021-05-31 11:15:52 +0800

    kmp算法完善

--- src/main/java/com/dair/sbxx/day9/KMP.java
+++ src/main/java/com/dair/sbxx/day9/KMP.java
@@ -1,41 +1,71 @@
 package com.dair.sbxx.day9;
 
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
+
 /**
  * @author Dair
  * @since
  */
 public class KMP {
 	
-	//BBC ABCDAB ABCDABCDABDE
-	//ABCDABD
+
 	public int solution(char[] text, char[] part) {
 		if (part.length == 0 || text.length == 0 || text.length < part.length) {
 			return -1;
 		}
-		int res = -1;
-		int index = 0;
-		while (index < text.length) {
-			
-			if (index + part.length < text.length) {
-				boolean flag = true;
-				int i = 0;
-				for (i = 0; i < part.length && flag; i++) {
-					if (text[index + i] != part[i]) {
-						flag = false;
-					}
+		int[] next = getNext(String.valueOf(part));
+		
+		for (int i = 0, j = 0; i < text.length; ) {
+			if (text[i] != part[j]) {
+				if (next[j] == -1) {
+					i++;
+				} else {
+					i = i - next[j];
+					j = 0;
 				}
-				if (flag) {
-					res = index;
-				} else if (!flag && i == 0) {
-					index++;
-				} else if (!flag && i > 0) {
-//					index+=i+1-
-					//TODO 获取部门匹配值
+			} else {
+				if (j == part.length - 1) {
+					return i - part.length + 1;
+				} else {
+					i++;
+					j++;
 				}
 			}
-			
-			
+		}
+		return -1;
+	}
+	
+	/**
+	 * 获取最匹配子串
+	 *
+	 * @param b
+	 * @return
+	 */
+	public int[] getNext(String b) {
+		int len = b.length();
+		int j = 0;
+		//next表示长度为i的字符串前缀和后缀的最长公共部分，从1开始
+		int next[] = new int[len + 1];
+		next[0] = -1;
+		next[1] = 0;
+		//i表示字符串的下标，从0开始
+		for (int i = 1; i < len; i++) {
+			//j在每次循环开始都表示next[i]的值，同时也表示需要比较的下一个位置
+			while (j > 0 && b.charAt(i) != b.charAt(j)) {
+				j = next[j];
+			}
+			if (b.charAt(i) == b.charAt(j)) {
+				j++;
+			}
+			next[i + 1] = j;
 		}
 		
+		return next;
+	}
+	
+	
+	public static void main(String[] args) {
+		KMP kmp = new KMP();
+		System.out.println(kmp.solution("BBC ABCDAB ABCDABCDABDE".toCharArray(), "ABCDABD".toCharArray()));
 	}
 }
