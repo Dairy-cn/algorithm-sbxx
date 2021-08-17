813e3bc8b5fd18cabddf6a9073843b08361f3e66
dair <2997266712@qq.com>
2021-08-17 15:38:09 +0800

    excutors

---
+++ src/main/java/com/dair/sbxx/day13/MarkWordTest.java
@@ -0,0 +1,19 @@
+package com.dair.sbxx.day13;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/**
+ * @author Dair
+ * @since
+ */
+public class MarkWordTest {
+	
+	public static void main(String[] args) {
+		System.out.println( (-1)%2);
+	}
+	
+	
+}
