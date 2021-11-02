5cf515756393f174697ff2222e8ca303c93451d7
Dair <2997266712@qq.com>
2021-11-02 10:27:18 +0800

    删除指定的链表

---
+++ src/main/java/com/dair/sbxx/day48/ListNode.java
@@ -0,0 +1,19 @@
+package com.dair.sbxx.day48;
+
+import java.util.List;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class ListNode {
+
+
+	int val;
+	
+	ListNode next;
+	
+	public ListNode(int val) {
+		this.val = val;
+	}
+}
