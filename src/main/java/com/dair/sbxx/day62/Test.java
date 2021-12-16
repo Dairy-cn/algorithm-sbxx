27184360feb5eedea8d38c09c73e5b257e0fbb07
Dair <2997266712@qq.com>
2021-12-16 15:43:06 +0800

    拓扑排序å®Deg

--- src/main/java/com/dair/sbxx/day62/Test.java
+++ src/main/java/com/dair/sbxx/day62/Test.java
@@ -1,8 +1,7 @@
 package com.dair.sbxx.day62;
 
-import org.springframework.util.CollectionUtils;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,52 +12,42 @@ import java.util.Set;
  * @since
  */
 public class Test {
-	HashMap<Integer, Set<Integer>> map = new HashMap<>();
+	
 	
 	public int[] loudAndRich(int[][] richer, int[] quiet) {
+		List<Integer>[] list = new List[quiet.length];
+		
+		for (int i : quiet) {
+			list[i] = new ArrayList<>();
+		}
 		for (int[] ints : richer) {
-			Set<Integer> list = map.getOrDefault(ints[1], new HashSet<>());
-			list.add(ints[0]);
-			map.put(ints[1], list);
+			list[ints[1]].add(ints[0]);
 		}
-		int[] arr = new int[quiet.length];
+		int[] resArr = new int[quiet.length];
+		Arrays.fill(resArr, -1);
 		for (int i = 0; i < quiet.length; i++) {
-			Set<Integer> set = map.get(i);
-			Set<Integer> setList = getSetList(set);
-			setList.add(i);
-			int min = Integer.MAX_VALUE;
-			for (Integer integer : setList) {
-				int i1 = quiet[integer];
-				min = Math.min(min, i1);
-			}
-			arr[i] = min;
+			dfs(quiet, list, resArr, i);
 		}
-		return arr;
+		return resArr;
 	}
 	
-	private Set<Integer> getSetList(Set<Integer> set) {
-		Set<Integer> resSetList = new HashSet<>();
-		if (set == null || set.isEmpty()) {
-			return resSetList;
+	private void dfs(int[] quiet, List<Integer>[] list, int[] resArr, int x) {
+		if (resArr[x] != -1) {
+			return;
 		}
-		for (Integer integer : set) {
-			Set<Integer> set1 = map.get(integer);
-			if (set1 != null && !set1.isEmpty()) {
-				resSetList.addAll(set1);
-				Set<Integer> setList = getSetList(set1);
-				if (setList != null && !setList.isEmpty()) {
-					resSetList.addAll(setList);
-				}
+		resArr[x] = x;
+		for (Integer y : list[x]) {
+			dfs(quiet, list, resArr, y);
+			if (quiet[resArr[y]] < quiet[resArr[x]]) {
+				resArr[x] = resArr[y];
 			}
 		}
-		return resSetList;
 	}
-	
 	
 	public static void main(String[] args) {
-		int[] ints = new Test().loudAndRich(new int[][]{{1, 0}, {2, 1}, {3, 1}, {3, 7}, {4, 3}, {5, 3}, {6, 3}}, new int[]{3, 2, 5, 4, 6, 1, 7, 0});
-		for (int anInt : ints) {
-			System.out.println(anInt);
-		}
-    	}
+		//[[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]]
+		//[3,2,5,4,6,1,7,0]
+		new Test().loudAndRich(new int[][]{{1,0},{2,1},{3,1},{3,7},{4,3},{5,3},{6,3}},new int[]{3,2,5,4,6,1,7,0});
+	}
+	
 }
